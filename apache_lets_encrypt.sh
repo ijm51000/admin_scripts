cefff325963cfdc86d15bcdb89d5cf5f9f1a045b
Ian Mackyd <ianmac51@gmail.com>
2019-08-18 21:42:35 +0100

    Update apache_lets_encrypt.sh

--- apache_lets_encrypt.sh
+++ apache_lets_encrypt.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 DOMAIN=$1
 EMAIL=$2
-add-apt-repository -y ppa:certbot/certbot
-apt install -y  python-certbot-apache
-certbot --apache -d ${DOMAIN} -d ${DOMAIN} --non-interactive --agree-tos -m ${EMAIL}
+sudo add-apt-repository -y ppa:certbot/certbot
+sudo apt install -y  python-certbot-apache
+sudo certbot --apache -d ${DOMAIN} -d ${DOMAIN} --non-interactive --agree-tos -m ${EMAIL}
 echo "testing cert renewal"
 certbot renew --dry-run
 curl https://${DOMAIN}
