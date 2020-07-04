43c56ed897dbe091da8e753901acc2ed45e10e56
ijm51000 <ianmac@gmail.com>
2020-07-04 20:35:09 +0100

    Change file download location

--- amq_install.sh
+++ amq_install.sh
@@ -1,7 +1,12 @@
+set -x
 JAVA_LOCATION=/usr/java/az_jdk-8/bin/java
 WRAPPER=/apps/u01/activemq/bin/linux-x86-64/wrapper.conf
 AMQ_CLIENT_ALL=activemq-all-5.13.4.jar
 AMQ_CLIENT_CLIENT=activemq-client-5.13.4.jar
+JAVA_FILE=amazon-corretto-8-x64-linux-jdk.tar.gz
+AMQ_FILE=apache-activemq-5.13.4-bin.tar.gz
+AMQ_VERSION=5.13.4
+AMQ_PATH=https://archive.apache.org/dist/activemq
 #sudo -u uat1 /apps/u01/activemq/bin/linux-x86-64/activemq stop
 sudo systemctl stop activemq.service
 sudo rm -rf /usr/java/az_jdk-8
@@ -10,18 +15,26 @@ sudo rm -rf /apps/u01/mule
 sudo userdel -r uat1
 #rm -f ${HOME}/amazon-corretto-8-x64-linux-jdk.tar.gz 
 #rm -f ${HOME}/apache-activemq-5.13.4-bin.tar.gz
+if [[ ! -e "/tmp/${JAVA_FILE}" ]]
+	then 
+		wget https://corretto.aws/downloads/latest/${JAVA_FILE} -O /tmp/${JAVA_FILE} 
+	fi
+if [[ ! -e "/tmp/${AMQ_FILE}" ]]
+	then
+		wget ${AMQ_PATH}/${AMQ_VERSION}/${AMQ_FILE} -O /tmp/${AMQ_FILE}
+	fi
 
-#wget https://corretto.aws/downloads/latest/amazon-corretto-8-x64-linux-jdk.tar.gz 
-#wget https://archive.apache.org/dist/activemq/5.13.4/apache-activemq-5.13.4-bin.tar.gz
+# wget https://archive.apache.org/dist/activemq/5.13.4/apache-activemq-5.13.4-bin.tar.gz
 echo "Unpacking JDK"
-tar -xf amazon-corretto-8-x64-linux-jdk.tar.gz
+tar -xf /tmp/${JAVA_FILE} -C /tmp
+
 echo  "Unpacking ActiveMQ"
-tar -xf apache-activemq-5.13.4-bin.tar.gz
+tar -xf /tmp/${AMQ_FILE} -C /tmp #apache-activemq-5.13.4-bin.tar.gz
 sudo mkdir -p /usr/java
-sudo mv amazon-corretto-8.252.09.1-linux-x64 /usr/java/az_jdk-8
+sudo mv /tmp/amazon-corretto-8.252.09.1-linux-x64 /usr/java/az_jdk-8
 
 sudo useradd -m  -s $(which bash)  -d /apps/u01 uat1
-sudo mv apache-activemq-5.13.4 /apps/u01/activemq
+sudo mv /tmp/apache-activemq-5.13.4 /apps/u01/activemq
 sudo chown -R uat1:uat1 /apps/u01/activemq
 #sudo sed -i.orig 's|\(wrapper.java.command=\).*|\1/usr/java/az_jdk-8/bin/java|g' ${WRAPPER} 
 sudo sed -i.orig "s|\(wrapper.java.command=\).*|\1${JAVA_LOCATION}|g" ${WRAPPER} 
